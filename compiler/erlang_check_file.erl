2d2ad586d8b8f73be58db4d7dea04a99f340fea0
Richard Jones <rj@metabrew.com>
2012-01-04 12:46:26 +0000

    use erl_opts from rebar.config, if present, for additional include dirs and other settings needed for compilation

--- compiler/erlang_check_file.erl
+++ compiler/erlang_check_file.erl
@@ -2,7 +2,16 @@
 -export([main/1]).
 
 main([File_Name]) ->
-    compile:file(File_Name, [warn_obsolete_guard,
-                            warn_unused_import,
-                            warn_shadow_vars, warn_export_vars,
-                            strong_validation, report]).
+    Default = [     warn_obsolete_guard,
+                    warn_unused_import,
+                    warn_shadow_vars, warn_export_vars,
+                    strong_validation, report],
+    case filelib:is_file("rebar.config") of
+        true ->
+            {ok, Terms} = file:consult("rebar.config"),
+            Opts = proplists:get_value(erl_opts, Terms, []),
+        	compile:file(File_Name, Default ++ Opts);
+        false ->
+            compile:file(File_Name, Default)
+    end.
+    
